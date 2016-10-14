bbae75002ba9966d9d107cce53f38ef403d5ddb3
vedhavyas singareddi <vedhavyas.singareddi@gmail.com>
2016-10-14 05:58:51 +0530

    some more tests

--- utils/urls.go
+++ utils/urls.go
@@ -1,18 +1,23 @@
 package utils
 
-import "net/url"
+import (
+	"net/url"
+	"strings"
+)
 
 //MakeAbsolute returns an absolute url of the extracted href
 //returns default string value of failed to resolve to absolute
 func ResolveURL(baseURL, href string) (*url.URL, *url.URL, error) {
-	uri, err := url.Parse(href)
-	if err != nil {
-		return nil, nil, err
-	}
 	baseURI, err := url.Parse(baseURL)
 	if err != nil {
 		return nil, nil, err
 	}
+
+	uri, err := url.Parse(href)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	uri = baseURI.ResolveReference(uri)
 	return baseURI, uri, nil
 }
@@ -23,6 +28,14 @@ func ResolveURLS(baseurl string, hrefs []string, restrictToDomain bool) ([]strin
 		baseURI, uri, err := ResolveURL(baseurl, href)
 		if err != nil {
 			return resolvedURLS, err
+		}
+
+		if uri.Scheme == "" || uri.Host == "" {
+			continue
+		}
+
+		if strings.Contains(uri.Path, baseURI.Host) {
+			continue
 		}
 
 		if baseURI.Host != uri.Host && restrictToDomain {
--- utils/urls_test.go
+++ utils/urls_test.go
@@ -1,7 +1,152 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResolveURL(t *testing.T) {
+	cases := []struct {
+		BaseURL  string
+		Href     string
+		Expected string
+		Error    bool
+	}{
+		{
+			BaseURL:  "http://www.test.com",
+			Href:     "/page/1",
+			Expected: "http://www.test.com/page/1",
+			Error:    false,
+		},
+
+		{
+			BaseURL:  "https://www.test.com",
+			Href:     "/page/2",
+			Expected: "https://www.test.com/page/2",
+			Error:    false,
+		},
+
+		{
+			BaseURL:  "",
+			Href:     "page",
+			Expected: "/page",
+			Error:    false,
+		},
+
+		{
+			BaseURL:  "www.test.com",
+			Href:     "page",
+			Expected: "/page",
+			Error:    false,
+		},
+
+		{
+			BaseURL:  "https://www.test.com",
+			Href:     "https://www.test2.com/page/2",
+			Expected: "https://www.test2.com/page/2",
+			Error:    false,
+		},
+
+		{
+			BaseURL:  "http://www.test.com",
+			Href:     "https://www.test2.com/page/2",
+			Expected: "https://www.test2.com/page/2",
+			Error:    false,
+		},
+	}
+
+	for _, testCase := range cases {
+		_, hrefURI, err := ResolveURL(testCase.BaseURL, testCase.Href)
+		if testCase.Error && err == nil {
+			assert.Fail(t, "Expected to fail")
+			continue
+		}
+		assert.Equal(t, testCase.Expected, hrefURI.String())
+	}
+}
 
 func TestResolveURLS(t *testing.T) {
+	cases := []struct {
+		BaseURL     string
+		Hrefs       []string
+		Expected    []string
+		DomainLimit bool
+		Error       bool
+	}{
+		{
+			BaseURL: "http://www.test.com",
+			Hrefs: []string{
+				"/page/1",
+				"/page/2",
+				"https://test.com/page/3",
+				"http://test2.com/page/4",
+				"https://test2.com/page/5",
+			},
+			Expected: []string{
+				"http://www.test.com/page/1",
+				"http://www.test.com/page/2",
+				"https://test.com/page/3",
+				"http://test2.com/page/4",
+				"https://test2.com/page/5",
+			},
+			Error: false,
+		},
 
+		{
+			BaseURL: "http://www.test.com",
+			Hrefs: []string{
+				"/page/1",
+				"/page/2",
+				"https://test.com/page/3",
+				"http://test2.com/page/4",
+				"https://test2.com/page/5",
+			},
+			Expected: []string{
+				"http://www.test.com/page/1",
+				"http://www.test.com/page/2",
+			},
+			Error:       false,
+			DomainLimit: true,
+		},
+
+		{
+			BaseURL: "www.test.com",
+			Hrefs: []string{
+				"/page/1",
+				"/page/2",
+				"https://test.com/page/3",
+				"http://test2.com/page/4",
+				"https://test2.com/page/5",
+			},
+			Expected: []string{},
+			Error:    false,
+		},
+
+		{
+			BaseURL: "http://www.test.com",
+			Hrefs: []string{
+				"/page/1",
+				"/page/2",
+				"www.test.com/page/3",
+			},
+			Expected: []string{
+				"http://www.test.com/page/1",
+				"http://www.test.com/page/2",
+			},
+			Error: false,
+		},
+	}
+
+	for _, testCase := range cases {
+		hrefs, err := ResolveURLS(testCase.BaseURL, testCase.Hrefs, testCase.DomainLimit)
+		if testCase.Error && err == nil {
+			assert.Fail(t, "Expected to fail")
+			continue
+		}
+		assert.Equal(t, len(testCase.Expected), len(hrefs))
+		for _, expectedHref := range testCase.Expected {
+			assert.Contains(t, hrefs, expectedHref)
+		}
+	}
 }
